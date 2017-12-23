e22fb0a98d93b33c0576d0afa5c84bca3ea95807
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-23 11:45:30 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,9 +1,9 @@
-class Puppy 
+class Puppy
   attr_accessors :name, :breed, :age
 
   def initalize(name,breed,age)
-    @name = name 
-    @breed = breed 
+    @name = name
+    @breed = breed
     @age = age
-  end 
+  end
 end 
